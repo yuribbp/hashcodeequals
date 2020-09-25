7540deab7c84ab4b0703db3101804e1e0cb64664
Yuri Baldan <yuribbp@gmail.com>
2020-09-25 14:40:33 -0300

    Hashcode

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,7 +6,8 @@ public class Program {
 
 		String a = "Maria";
 		String b = "Alex";
-		System.out.println(a.equals(b));
+		System.out.println(a.hashCode());
+		System.out.println(b.hashCode());
 	}
 
 }
