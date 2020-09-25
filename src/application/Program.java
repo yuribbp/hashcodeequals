d1733c0435cccdec4010c7a64ecc53cf4fe2d43a
Yuri Baldan <yuribbp@gmail.com>
2020-09-25 15:00:24 -0300

    Using same name AND email -> hashcode and equals

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,15 +10,18 @@ public class Program {
 		Client c2 = new Client("Alex", "alex@gmail.com");
 		Client c3 = new Client("Bob", "maria@gmail.com");
 		Client c4 = new Client("Maria", "maria2@gmail.com");
+		Client c5 = new Client("Maria", "maria@gmail.com");
 		
 		System.out.println("C1 = " + c1.getName() + " = " + c1.hashCode());
 		System.out.println("C2 = " + c2.getName() + " = " + c2.hashCode());
 		System.out.println("C3 = " + c3.getName() + " = " + c3.hashCode());
 		System.out.println("C4 = " + c4.getName() + " = " + c4.hashCode());
+		System.out.println("C5 = " + c5.getName() + " = " + c5.hashCode());
 		System.out.println();
 		System.out.println("C1: " + c1.getName() + " equals C2: " + c2.getName() + " = " + c1.equals(c2));
 		System.out.println("C1: " + c1.getName() + " equals C3: " + c3.getName() + " = " + c1.equals(c3));
 		System.out.println("C1: " + c1.getName() + " equals C4: " + c4.getName() + " = " + c1.equals(c4));
+		System.out.println("C1: " + c1.getName() + " equals C5: " + c5.getName() + " = " + c1.equals(c5));
 		
 	}
 
